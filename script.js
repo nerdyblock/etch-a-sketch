cbc34f0fdbc1346f4cf895fb6c63816fb2647054
Akash Pal <creativeakashpal@gmail.com>
2022-05-18 19:14:15 +0530

    added clear function and cleaned the code

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let container = document.querySelector('.container');
 let gridSize = 0;
-let slider = document.querySelector('.myslider');
-let output = document.querySelector('#demo');
 let grid;
+
 let colorPicker = document.getElementById('color');
+// default color of pen
 let ink = 'black';
-let random = document.getElementById('random-color');
-let randomFlag = false;
-let eraseFlag = false;
-let gridFlag = true;
+
+colorPicker.addEventListener('input', () => ink = colorPicker.value);
+
 let showGrid = document.querySelector('#grid');
+let gridFlag = true;
+
 showGrid.classList.add('btn-on');
 
 showGrid.addEventListener('click', () => {
@@ -23,7 +24,27 @@ showGrid.addEventListener('click', () => {
         showGrid.classList.add('btn-on');
         addGridBorder();
     }
-})
+});
+
+// hide grid lines
+function removeGridBorder() {
+    if(gridFlag === false) {
+        grid.forEach(item => 
+            item.classList.remove('box'));
+    }
+}
+
+// show grid lines 
+function addGridBorder() {
+    if(gridFlag === true) {
+        grid.forEach(item => 
+            item.classList.add('box'));
+    }
+}
+
+
+let random = document.getElementById('random-color');
+let randomFlag = false;
 
 random.addEventListener('click', () =>  {
     if(randomFlag){
@@ -38,11 +59,14 @@ random.addEventListener('click', () =>  {
     }
 });
 
-color.addEventListener('input', () => ink = colorPicker.value);
+// random color generater using rgb
+function randomColor(){
+    return 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')';
+}
 
-output.textContent = slider.value;
-
+// for eraser button
 let erase = document.getElementById('erase');
+let eraseFlag = false;
 erase.addEventListener('click', () => {
     if(eraseFlag){
         eraseFlag = false;
@@ -56,67 +80,77 @@ erase.addEventListener('click', () => {
     }
 });
 
+// for slider input for grid size
+let slider = document.querySelector('.myslider');
+// for text output of slider value
+let output = document.querySelector('#demo');
+
+output.textContent = `${slider.value} x ${slider.value}`;
+
 slider.addEventListener('input', function() {
-    output.textContent = this.value;
+    output.textContent = `${this.value} x ${this.value}`;
     gridSize = this.value;
-    clearGrid();
     createGrid(gridSize);
 });
 
+let clear = document.querySelector('#clear');
+// clear the screen 
+clear.addEventListener('click', () =>  {
+    grid.forEach(item => {
+        item.style.backgroundColor = 'white';
+    })
+});
+
+// create grid default size 16*16
 function createGrid(gridSize=16) {
+    // remove previously created grid(divs) 
+    clearGrid();
+    // make new grid(divs) according to new gridSize
     for(let i=0; i<gridSize**2; i++) {
         let box = document.createElement('div');
+        gridFlag = true;
+        showGrid.classList.add('btn-on');
+        box.setAttribute('id', 'box');
         box.classList.add('box');
-        box.setAttribute('id', 'box')
-        box.style.width = (600/gridSize) + 'px';
-        box.style.height = (600/gridSize) + 'px';
+        // width and height of one grid
+        box.style.width = (620/gridSize) + 'px';
+        box.style.height = (620/gridSize) + 'px';
         container.appendChild(box);
     }
 
     grid = document.querySelectorAll('#box');
     grid.forEach(item => {
-        item.addEventListener('mouseenter', colorGrid);
+        // color the grid when mouse moves or mouse is pressed down
+        item.addEventListener('mousemove', colorGrid);
         item.addEventListener('mousedown', colorGrid);
     });
 }
 
+
 function colorGrid(e) {
+    // color only if a mouse button is pressed
     if(e.buttons > 0)
-        
         if(eraseFlag) {
+            // if eraseFlag is true make the pen color to white
             this.style.backgroundColor = 'white';
         }
         else if(randomFlag) {
+            // if randomFlag is true generate random color every grid
             this.style.backgroundColor = randomColor();
         }
         else {
+            // make the grid to whatever the color user has selected (default black)
             this.style.backgroundColor = ink;
         }
 }
 
 function clearGrid() {
+    // clear grids, called when the grid size is changed
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }      
 }
 
-function removeGridBorder() {
-    if(gridFlag === false) {
-        grid.forEach(item => 
-            item.classList.remove('box'));
-    }
-}
-
-function addGridBorder() {
-    if(gridFlag === true) {
-        grid.forEach(item => 
-            item.classList.add('box'));
-    }
-}
-
-function randomColor(){
- return 'rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')';
-}
 
 createGrid();
 
